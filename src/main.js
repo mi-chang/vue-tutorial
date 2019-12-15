33ee5d008a65551518d0b270745eb49f32c67a22
mi-chang <mihchang@gmail.com>
2019-12-15 15:35:29 +0900

    Implement Login and LoginForm

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueChatScroll from 'vue-chat-scroll'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/ldbtn.min.css'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VueChatScroll)
 
 new Vue({
   router,
